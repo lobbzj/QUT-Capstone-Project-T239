da2da573e3b7afd16a23e5fdcf05d5c3c3aa3471
Jason Zhang <n11255838@qut.edu.au>
2024-09-10 16:07:03 +1000

    regulate the file structure; added a base.html file to the templates folder; added some images to the static/img folder;

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask 
+
+def create_app():
+    print(__name__)  
+    app = Flask(__name__)  
+    app.debug = True
+    app.secret_key = 'asecret'
+    from . import views
+    app.register_blueprint(views.mainbp)
+    return app
