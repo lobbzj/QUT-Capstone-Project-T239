2971824e215741fdea5556b063c62022b7b3d24e
Minh Le <daminhster@gmail.com>
2025-04-03 16:56:56 +1000

    Front-end implemented for all products filter function

---
+++ app/static/js/filter.js
@@ -0,0 +1,8 @@
+function toggleFilterMenu() {
+    const filterMenu = document.getElementById('filterMenu');
+    if (filterMenu.style.display === 'block') {
+        filterMenu.style.display = 'none';
+    } else {
+        filterMenu.style.display = 'block';
+    }
+}
