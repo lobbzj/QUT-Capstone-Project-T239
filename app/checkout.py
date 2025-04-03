d2dfaa0fd678e29133cdc83a3ea3ef32733971b2
JASON ZHANG <128665714+lobbzj@users.noreply.github.com>
2025-04-03 17:00:06 +1000

    Delete app directory

--- app/checkout.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint, render_template, request, session, redirect, url_for, flash
-checkoutbp = Blueprint('checkout', __name__, url_prefix='/checkout')
-
-@checkoutbp.route('/')
-def show():
-    return render_template('checkout.html')
--- app/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, request, session
-from .models import User, Product, Comment, Order, Payment
-from . import db
-
-mainbp = Blueprint('main', __name__)
-
-@mainbp.route('/')
-def index():
-    return render_template('index.html')
