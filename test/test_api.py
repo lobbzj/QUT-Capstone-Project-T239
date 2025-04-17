37528fe5e69360482e7d1768f282ce9c9b1d5ea4
Jonas <yipchunlong888@gmail.com>
2025-04-17 14:35:24 +1000

    modifying

--- test/test_api.py
+++ test/test_api.py
@@ -1,1 +1,12 @@
+import requests
+
+def test_homepage():
+    response = requests.get("http://localhost:5000/")
+    assert response.status_code == 200
+    assert "Testing" in response.text
+
+def test_greet():
+    response = requests.get("http://localhost:5000/")
+    assert response.status_code == 200
+    assert response.json()["message"] == "Testing done"
 
---
+++ test/test_ui.py
@@ -0,0 +1,16 @@
+import selenium
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+def test_home_ui():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("http://localhost:5000")
+    assert "Hello from Python" in driver.page_source
+    driver.quit()
