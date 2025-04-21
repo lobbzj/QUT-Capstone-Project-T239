3ab53648e57f94ac2ca1829670c14f02bf7ba28c
Jonas <yipchunlong888@gmail.com>
2025-04-21 20:28:50 +1000

    updates on confirmation page

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\damin\Documents\IFB399 Project Local\QUT-Capstone-Project-T239\.venv
+set VIRTUAL_ENV=C:\Users\yipch\Desktop\QUT-Capstone-Project-T239\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
