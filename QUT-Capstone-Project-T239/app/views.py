9360253beb2fdf3507651826749c730ec23210f2
Minh le <daminhster@gmail.com>
2025-05-06 14:30:44 +1000

    Fixed the filter function. Now displaying products inline with the row when available instead of spaced out when filtered

--- QUT-Capstone-Project-T239/app/views.py
+++ QUT-Capstone-Project-T239/app/views.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, render_template, request, session
-from .models import User, Product, Comment, Order, Payment
+from app.models import User, Product, Comment, Order, Payment
 from . import db
 
 mainbp = Blueprint('main', __name__)
 
 @mainbp.route('/')
 def index():
+    # Fetch all products
     products = Product.query.all()
-    return render_template('index.html', products=products)
+
+    # Fetch unique categories for filtering
+    categories = {product.category for product in products if product.category}
+    return render_template('index.html', products=products, categories=categories)
