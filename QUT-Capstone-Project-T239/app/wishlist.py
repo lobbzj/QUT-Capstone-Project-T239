c088317c7329357a6cda71205663ef235a95f55b
Minh Le <daminhster@gmail.com>
2025-05-15 10:51:31 +1000

    Login only for wishlist. 'Start shopping' button to 'all products page'. New hero message at the top.

--- QUT-Capstone-Project-T239/app/wishlist.py
+++ QUT-Capstone-Project-T239/app/wishlist.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, session, redirect, url_for, flash, render_template
 from app.models import Product  # Import your Product model
+from flask_login import login_required, current_user
 
 wishlist_bp = Blueprint('wishlist', __name__)
 
 @wishlist_bp.route('/wishlist/add/<int:id>', methods=['POST'])
+@login_required
 def add_to_wishlist(id):
     product = Product.query.get_or_404(id)
     if 'wishlist' not in session:
@@ -17,6 +19,7 @@ def add_to_wishlist(id):
     return redirect(url_for('product.show', id=id))
 
 @wishlist_bp.route('/wishlist')
+@login_required
 def show_wishlist():
     if 'wishlist' not in session or not session['wishlist']:
         wishlist_items = []
@@ -25,6 +28,7 @@ def show_wishlist():
     return render_template('wishlist.html', wishlist_items=wishlist_items)
 
 @wishlist_bp.route('/wishlist/remove/<int:id>', methods=['POST'])
+@login_required
 def remove_from_wishlist(id):
     if 'wishlist' in session and id in session['wishlist']:
         session['wishlist'].remove(id)
