27872c628be60e41e6f1222a669719c8d8253114
Jason Zhang <n11255838@qut.edu.au>
2025-05-15 11:38:00 +1000

    fixed the issue that a logged-in user can access login page

--- QUT-Capstone-Project-T239/app/__init__.py
+++ QUT-Capstone-Project-T239/app/__init__.py
@@ -73,6 +73,14 @@ def create_app():
       year = datetime.datetime.today().year
       return dict(year=year)
     
+    @app.after_request
+    def add_header(response):
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '0'
+        return response
+
+    
     with app.app_context():
       db.create_all()
     
--- QUT-Capstone-Project-T239/app/auth.py
+++ QUT-Capstone-Project-T239/app/auth.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
-from flask_login import login_user, login_required, logout_user
+from flask import Blueprint, render_template, redirect, url_for, flash, request, session
+from flask_login import login_user, login_required, logout_user, current_user
 from flask_bcrypt import generate_password_hash, check_password_hash
 from .forms import LoginForm, RegisterForm
 from .models import User
+
+
 from . import db
 
 # Blueprint
@@ -11,38 +13,43 @@ authbp = Blueprint('auth', __name__)
 # Register route
 @authbp.route('/register', methods=['GET', 'POST'])
 def register():
-    register_form = RegisterForm()
-    if register_form.validate_on_submit():
-        uname = register_form.user_name.data
-        pwd = register_form.password.data
-        email = register_form.email_id.data
-        mobile = register_form.mobile.data
-        address = register_form.address.data
-        city = register_form.city.data
-        state = register_form.state.data
-        zip_code = register_form.zip_code.data
-        member_type = register_form.member_type.data
+    if current_user.is_authenticated:
+        flash('You are already logged in.', 'info')
+        return redirect(url_for('main.index'))
+    
+    else:
+        register_form = RegisterForm()
+        if register_form.validate_on_submit():
+            uname = register_form.user_name.data
+            pwd = register_form.password.data
+            email = register_form.email_id.data
+            mobile = register_form.mobile.data
+            address = register_form.address.data
+            city = register_form.city.data
+            state = register_form.state.data
+            zip_code = register_form.zip_code.data
+            member_type = register_form.member_type.data
 
-        # Combine address fields
-        full_address = f"{address}, {city}, {state} {zip_code}"
+            # Combine address fields
+            full_address = f"{address}, {city}, {state} {zip_code}"
 
-        # Check if user already exists
-        user = db.session.scalar(db.select(User).where(User.name == uname))
-        if user:
-            flash('Username already exists, please try another.', 'danger')
-            return redirect(url_for('auth.register'))
+            # Check if user already exists
+            user = db.session.scalar(db.select(User).where(User.name == uname))
+            if user:
+                flash('Username already exists, please try another.', 'danger')
+                return redirect(url_for('auth.register'))
 
-        # Hash the password
-        pwd_hash = generate_password_hash(pwd)
+            # Hash the password
+            pwd_hash = generate_password_hash(pwd)
 
-        # Create new user
-        new_user = User(name=uname, password_hash=pwd_hash, mobile=mobile, email=email, address=full_address, member_type=member_type)
-        db.session.add(new_user)
-        db.session.commit()
+            # Create new user
+            new_user = User(name=uname, password_hash=pwd_hash, mobile=mobile, email=email, address=full_address, member_type=member_type)
+            db.session.add(new_user)
+            db.session.commit()
 
-        flash('Registration successful! You can now log in.', 'success')
+            flash('Registration successful! You can now log in.', 'success')
 
-        return redirect(url_for('auth.login'))
+            return redirect(url_for('auth.login'))
         
 
     return render_template('user.html', form=register_form, heading='Register')
@@ -52,25 +59,30 @@ def register():
 # Login route
 @authbp.route('/login', methods=['GET', 'POST'])
 def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        user_name = login_form.user_name.data
-        password = login_form.password.data
-        member_type = login_form.member_type.data
+    if current_user.is_authenticated:
+        flash('You are already logged in.', 'info')
+        return redirect(url_for('main.index'))
+    
+    else:
+        login_form = LoginForm()
+        if login_form.validate_on_submit():
+            user_name = login_form.user_name.data
+            password = login_form.password.data
+            member_type = login_form.member_type.data
 
-        # Check if user exists
-        user = db.session.scalar(db.select(User).where(User.name == user_name))
+            # Check if user exists
+            user = db.session.scalar(db.select(User).where(User.name == user_name))
 
-        if user is None:
-            flash('Incorrect username', 'danger')
-        elif not check_password_hash(user.password_hash, password):
-            flash('Incorrect password', 'danger')
-        elif user.member_type != member_type:
-            flash('Incorrect member type', 'danger')
-        else:
-            login_user(user)
-            flash('Login successful!', 'success')
-            return redirect(url_for('main.index'))
+            if user is None:
+                flash('Incorrect username', 'danger')
+            elif not check_password_hash(user.password_hash, password):
+                flash('Incorrect password', 'danger')
+            elif user.member_type != member_type:
+                flash('Incorrect member type', 'danger')
+            else:
+                login_user(user)
+                flash('Login successful!', 'success')
+                return redirect(url_for('main.index'))
 
     return render_template('user.html', form=login_form, heading='Login', action='login')
 
@@ -80,6 +92,7 @@ def login():
 @login_required
 def logout():
     logout_user()
+    session.clear()
     flash('You have been logged out.', 'info')
     return redirect(url_for('auth.login'))
 
