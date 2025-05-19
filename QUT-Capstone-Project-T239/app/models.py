2559dffa8eb30068f5d027a33edbf57a8b29775a
Jonas <yipchunlong888@gmail.com>
2025-05-19 14:17:00 +1000

    Deleted cart table

--- QUT-Capstone-Project-T239/app/models.py
+++ QUT-Capstone-Project-T239/app/models.py
@@ -99,18 +99,7 @@ class Contact(db.Model):
         return f'<Contact {self.name}>'
     
 # added by Minh
-class Cart(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)  # Correct table name is 'users'
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)  # Correct table name is 'products'
-    quantity = db.Column(db.Integer, nullable=False, default=1)
-
-    # Relationships
-    product = db.relationship('Product', backref='cart_items')
-    user = db.relationship('User', backref='cart_items')
-
-    def __repr__(self):
-        return f"<Cart User {self.user_id} Product {self.product_id} Quantity {self.quantity}>"
+# cart table deleted by Jonas
     
     
 # added by Jonas
